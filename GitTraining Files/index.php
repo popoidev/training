d4650f28d6c742cbdf2baab0ca114d1a6e9e3212
popoidev <popoidev@gmail.com>
2020-01-11 11:54:28 +0800

    GitTraining 1.11

---
+++ GitTraining Files/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    //boni
+    // declare a variable assign value
+
+    //jen
+    // do something sa variable na ginawa ni boni 
+
+    //chris
+    // create a foreach loop 
+
+    //jm
+    // update ung foreach loop ni chris add an array with values
+
+    //paul
+    // echo ung foreach values
